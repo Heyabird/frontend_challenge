2179f13f46e4668afb1efa9d0763aa374a84f746
heya <heyabird@gmail.com>
2021-01-09 14:56:07 -0800

    query and place index copy data

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import BackgroundImage from 'gatsby-background-image'
 import Layout from "../components/layout"
 import { Highlight } from "../components/highlight"
 import Header from "../components/header"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import "../css/index.css"
 import "../css/global.css"
 
@@ -16,7 +18,7 @@ const IndexPage = ({ data }) => {
   const backgroundImageData = data.allContentfulLayoutBackgroundImage.edges[0].node.image.fluid
   const logoImageData = data.allContentfulNavItem.edges[4].node.image.svg.dataURI
   const highlightFeatureData = data.allContentfulLayoutHighlightedFeature.edges[0].node.feature
-  // console.log("logoImageData", logoImageData)
+  const layoutCopyData = data.allContentfulLayoutCopy.edges
   console.log("highlightFeatureData", highlightFeatureData)
 
   return (
@@ -31,13 +33,13 @@ const IndexPage = ({ data }) => {
 
         <div className="index-middle">
           <div className="hero-copy">
-            <p>America's #1 Rated Adventure Platform</p>
-            <h1>Take control of your weekends</h1>
-            <p>Find adventure anywhere.</p>
+            <p>{layoutCopyData[2].node.subHeadline}</p>
+            <h1>{layoutCopyData[2].node.headline}</h1>
+            <p>{layoutCopyData[2].node.shortDescription}</p>
           </div>
 
-          <a className="button primary">Get started</a>
-          <a className="button secondary">Request a demo</a>
+          <a className="button primary">{layoutCopyData[1].node.ctaTitle} <FontAwesomeIcon icon={faChevronRight} size="xs"/></a>
+          <a className="button secondary">{layoutCopyData[0].node.ctaTitle} <FontAwesomeIcon icon={faChevronRight} size="xs"/></a>
 
           <div className="highlight-section">
             <Highlight title={highlightFeatureData[0].title} imgSrc={highlightFeatureData[0].image.fluid.src} shortDescription={highlightFeatureData[0].shortDescription}/>
@@ -89,6 +91,20 @@ export const query = graphql`
         }
         title
         shortDescription
+      }
+    }
+
+    allContentfulLayoutCopy {
+      edges {
+        node {
+          title
+          shortDescription
+          subHeadline
+          headline
+          visualStyle
+          ctaTitle
+          type
+        }
       }
     }
 
