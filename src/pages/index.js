ef9fda8e38a615c8821d81d8ed486b15483a0378
heya <heyabird@gmail.com>
2021-01-08 17:20:40 -0800

    test adding image on gatsby thru contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Img from 'gatsby-image'
 
 import Layout from "../components/layout"
+import { node } from "prop-types"
 
 
 const IndexPage = ( {data} ) => (
   <Layout>
-    <h1>{data.allContentfulLayoutBackgroundImage.edges.[0].node.title}</h1>
+    <img src={data.allContentfulLayoutBackgroundImage.edges[0].node.image.fluid.src}/>
   </Layout>
 )
 
@@ -17,7 +19,14 @@ export const query = graphql`query MyQuery {
     edges {
       node {
         title
+        image {
+          fluid {
+            src
+          }
+        }
       }
     }
   }
-}`
+}
+
+`
