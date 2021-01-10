4bfd1a1f82949d96d634e9e43536d86e14aadac2
heya <heyabird@gmail.com>
2021-01-09 22:23:22 -0800

    refactor and pseudocode highlight.js

--- src/components/highlight.js
+++ src/components/highlight.js
@@ -1,23 +1,20 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
-import { graphql } from "gatsby"
-import "../css/highlight.css"
-import "../css/global.css"
+import { Link } from "gatsby"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import "../css/highlight.css"
+import "../css/global.css"
 
 export const Highlight = (props) => {
-  console.log("img src", props.imgSrc)
-    // const featureData = data.allContentfulFeature.nodes[0].title
-    return (
-      <>
-        <div id={props.mobileHighlight ? 'mobile-highlight' : null} className="highlight">
-          <img className="highlight-image" src={props.imgSrc}/>
-          <p className="highlight-title">{props.title}</p>
-          <p className="highlight-description">{props.shortDescription}</p>
-          <a href="#" className="highlight-learn-more">Learn more&nbsp; <FontAwesomeIcon icon={faChevronRight} size="xs"/></a>
-        </div>
-      </>
-    )
+  // console.log("img src", props.imgSrc)
+  return (
+    <>
+      <div id={props.mobileHighlight ? 'mobile-highlight' : null} className="highlight">
+        <img className="highlight-image" src={props.imgSrc}/>
+        <p className="highlight-title">{props.title}</p>
+        <p className="highlight-description">{props.shortDescription}</p>
+        <Link to="#" className="highlight-learn-more">Learn more&nbsp; <FontAwesomeIcon icon={faChevronRight} size="xs"/></Link>
+      </div>
+    </>
+  )
 }
