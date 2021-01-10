2014fd7e274200f091560aab9ab4c7114c6c6abf
heya <heyabird@gmail.com>
2021-01-10 12:18:33 -0800

    fix fontawesome server side rendering bug

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,13 @@ import { useStaticQuery, graphql } from "gatsby"
 import { Helmet } from "react-helmet"
 import "../css/layout.css"
 import "../css/background-image.css"
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+// (src: https://stackoverflow.com/questions/56334381/why-my-font-awesome-icons-are-being-displayed-big-at-first-and-then-updated-to-t)
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
