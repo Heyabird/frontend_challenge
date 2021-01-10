9cee42c3fc2448e34bb26d3337286965ee0e8bb3
heya <heyabird@gmail.com>
2021-01-09 16:10:24 -0800

    change padding on header and have it incorporate all nav items

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import { graphql } from "gatsby"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import "../css/global.css"
-
+import "../css/header.css"
 
 const Header = props => {
 
@@ -15,16 +15,15 @@ const Header = props => {
   const logoURI = props.logoURI
   console.log("props", logoURI)
   return (
-  <>
-
-        <img id="logo" src={props.logoImageData}/>
-        <nav id="nav">
-          <a>{props.navItemData[3].node.title}</a>
-          <a>{props.navItemData[2].node.title}</a>
-          <a>{props.navItemData[1].node.title}</a>
-          <a href="#" class="button primary">{props.navItemData[0].node.title} <FontAwesomeIcon icon={faChevronRight} size="xs"/></a>
-        </nav>
-  </>
+    <div id="header">
+      <a href="#"><img id="logo" src={props.logoImageData}/></a>
+      <nav id="nav">
+        <a>{props.navItemData[3].node.title}</a>
+        <a>{props.navItemData[2].node.title}</a>
+        <a>{props.navItemData[1].node.title}</a>
+        <a href="#" class="button primary">{props.navItemData[0].node.title} <FontAwesomeIcon icon={faChevronRight} size="xs"/></a>
+      </nav>
+    </div>
   )
 }
 
