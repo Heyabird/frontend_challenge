c610650cf77b82e02fd41e380fc4eb31f6d5b0c2
heya <heyabird@gmail.com>
2021-01-09 22:22:23 -0800

    refactor and pseudocode Header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import { graphql } from "gatsby"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import "../css/global.css"
@@ -9,30 +7,17 @@ import "../css/header.css"
 
 const Header = props => {
 
-  // const logoImageData = data.allContentfulNavItem.edges
-  console.log("props test", props.logoURI);
-  console.log("props test", props.test);
-  const logoURI = props.logoURI
-  console.log("props", logoURI)
   return (
     <div id="header">
       <a href="#"><img id="logo" src={props.logoImageData}/></a>
       <nav id="nav">
-        <a>{props.navItemData[3].node.title}</a>
-        <a>{props.navItemData[2].node.title}</a>
-        <a>{props.navItemData[1].node.title}</a>
+        <Link to="#">{props.navItemData[3].node.title}</Link>
+        <Link to="#">{props.navItemData[2].node.title}</Link>
+        <Link to="#">{props.navItemData[1].node.title}</Link>
         <a href="#" className="button primary">{props.navItemData[0].node.title}&nbsp; <FontAwesomeIcon icon={faChevronRight} size="xs"/></a>
       </nav>
     </div>
   )
 }
-
-// Header.propTypes = {
-//   siteTitle: PropTypes.string,
-// }
-
-// Header.defaultProps = {
-//   siteTitle: ``,
-// }
 
 export default Header
